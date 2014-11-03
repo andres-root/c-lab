ff5e55a5e2dcac6c31e45033ff18c39ad3d89266
andres-root <andres.root.coder@gmail.com>
2014-11-02 20:58:20 -0500

    update

--- say.c
+++ say.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    puts("Hello world.");
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 56789.4532;
+    char initial = 'A';
+    char first_name[] = "Andres";
+    char last_name[] = "Root";
+
+    printf("You are %d miles away.\n", distance);
+    printf("You have %f levels of power.\n", power);
+    printf("You have %f awesome super powers.\n", super_power);
+    printf("I have an initial %c.\n", initial);
+    printf("I have a first name %s.\n", first_name);
+    printf("I have a last name %s.\n", last_name);
+    printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
+
     return 0;
 }
