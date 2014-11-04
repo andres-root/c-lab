9462467ba3fdb49ee15ebdbd9fd042d3c3523627
andres-root <andres.root.coder@gmail.com>
2014-11-03 20:35:43 -0500

    update

--- test.c
+++ test.c
@@ -1,49 +1,22 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    int numbers[4] = {0};
-    char name[4] = {'a'};
+int main(int argc, char *argv[])
+{
+    int i = 0;
 
-    printf("numbers: %d %d %d %d\n",
-            numbers[0], numbers[1],
-            numbers[2], numbers[3]);
+    for(i = 1; i < argc; i++) {
+        printf("arg %d: %s\n", i, argv[i]);
+    }
 
-    printf("name each: %c %c %c %c\n",
-            name[0], name[1],
-            name[2], name[3]);
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", "Texas"
+    };
+    int num_states = 4;
 
-    printf("name: %s\n", name);
-
-    numbers[0] = 1;
-    numbers[1] = 2;
-    numbers[2] = 3;
-    numbers[3] = 4;
-
-    name[0] = 'A';
-    name[1] = 'N';
-    name[2] = 'd';
-    name[3] = 'r';
-    name[4] = 'e';
-    name[5] = 's';
-    name[6] = '\0';
-
-    printf("numbers: %d %d %d %d\n",
-            numbers[0], numbers[1],
-            numbers[2], numbers[3]);
-
-    printf("name each: %c %c %c %c\n",
-            name[0], name[1],
-            name[2], name[3]);
-
-    printf("name: %s\n", name);
-
-    char *another = "Andres";
-
-    printf("another: %s\n", another);
-
-    printf("another each: %c %c %c %c\n",
-            another[0], another[1],
-            another[2], another[3]);
+    for(i = 0; i < num_states; i++) {
+        printf("state %d: %s\n", i, states[i]);
+    }
 
     return 0;
 }
